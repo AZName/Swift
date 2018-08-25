d8d7842a6bc37cb13e36e1698f89b8755929172d
阿振 <964190389@qq.com>
2018-08-25 17:33:48 +0800

    集合

---
+++ SwiftStudy.playground/Pages/day4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+//集合
+
+
+var someInts = [Int]()
+print("someInts is of type [Int] with \(someInts.count) items.")
+// prints "someInts is of type [Int] with 0 items."
+
+
+var treeDoubles = Array(repeating: 0.0, count: 3)
+
+
+print(treeDoubles)
+
+
+var otherTreeDoubles = Array(repeating: 3.0, count: 3)
+//两个数组相加是元素的数量增多
+let sixDoutbles = treeDoubles + otherTreeDoubles
+//连个各有3个元素的数组加一起变成了一个新的六个元素的数组
+print(sixDoutbles)
+
+print(sixDoutbles.count)
+
+
+
+
+//: [Next](@next)
